73017075fc6121eac6d0276d2598e92a74eadbdd
Luis Bravo <luis.g.bravo1@gmail.com>
2020-03-27 10:00:41 -0600

    Last mod

--- Coding-Challenge-3/js/index.js
+++ Coding-Challenge-3/js/index.js
@@ -18,41 +18,44 @@ function getFizz() {
 
     let input = document.querySelector('.fizzBuzzNumber');
     console.log(input.value);
+    if (input.value > 1) {
+      let results = document.querySelector('.resultsSection');
+      results.innerHTML = ''
 
-    let results = document.querySelector('.resultsSection');
-    results.innerHTML = ''
-
-    for(num = 1; num <= input.value; num++) {
-      if (num % 3 == 0 && num % 5 == 0) {
-        results.innerHTML += 
-        `
-          <div class="fizz-buzz-item fizzbuzz">
-          fizzbuzz
-          </div>
-        `;
-      } else if (num % 5 == 0) {
-        results.innerHTML += 
-        `
-          <div class="fizz-buzz-item buzz">
-          buzz
-          </div>
-        `;
-      } else if (num % 3 == 0) {
-        results.innerHTML += 
-        `
-          <div class="fizz-buzz-item fizz">
-          fizz
-          </div>
-        `;
-      } else {
-        results.innerHTML += 
-        `
-          <div class="fizz-buzz-item">
-        ` + num + 
-        `
-          </div>
-        `;
+      for(num = 1; num <= input.value; num++) {
+        if (num % 3 == 0 && num % 5 == 0) {
+          results.innerHTML += 
+          `
+            <div class="fizz-buzz-item fizzbuzz">
+            fizzbuzz
+            </div>
+          `;
+        } else if (num % 5 == 0) {
+          results.innerHTML += 
+          `
+            <div class="fizz-buzz-item buzz">
+            buzz
+            </div>
+          `;
+        } else if (num % 3 == 0) {
+          results.innerHTML += 
+          `
+            <div class="fizz-buzz-item fizz">
+            fizz
+            </div>
+          `;
+        } else {
+          results.innerHTML += 
+          `
+            <div class="fizz-buzz-item">
+          ` + num + 
+          `
+            </div>
+          `;
+        }
       }
+    } else {
+      console.log('Positive only')
     }
   })
 

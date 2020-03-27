d070ab6c7af9a14ca16c198c4ae11082660f92f1
Luis Bravo <luis.g.bravo1@gmail.com>
2020-03-27 09:51:35 -0600

    Exam

---
+++ Coding-Challenge-1/README.md
@@ -0,0 +1,9 @@
+<ol>
+  <li>git init</li>
+  <li>git remote add origin https://github.com/luisbravo1/First-Partial-Exam-1.git</li>
+  <li>git pull origin master</li>
+  <li>touch README.md</li>
+  <li>git add .</li>
+  <li>git commit -m "First Commit"</li>
+  <li>git push -u origin master</li>
+</ol>
---
+++ Coding-Challenge-3/js/index.js
@@ -0,0 +1,66 @@
+function deleteFizz() {
+  let fizz = document.getElementsByClassName('fizz-buzz-item');
+
+  for (let i=0; i < fizz.length; i++) {
+    fizz[i].addEventListener( 'click', ( event ) => {
+      let currFizz = event.target.id
+      console.log(currFizz)
+      currFizz.classList.add('hidden')
+    })
+  }
+}
+
+function getFizz() {
+  let btn = document.querySelector('.sub');
+
+  btn.addEventListener( 'click', ( event ) => {
+    event.preventDefault();
+
+    let input = document.querySelector('.fizzBuzzNumber');
+    console.log(input.value);
+
+    let results = document.querySelector('.resultsSection');
+    results.innerHTML = ''
+
+    for(num = 1; num <= input.value; num++) {
+      if (num % 3 == 0 && num % 5 == 0) {
+        results.innerHTML += 
+        `
+          <div class="fizz-buzz-item fizzbuzz">
+          fizzbuzz
+          </div>
+        `;
+      } else if (num % 5 == 0) {
+        results.innerHTML += 
+        `
+          <div class="fizz-buzz-item buzz">
+          buzz
+          </div>
+        `;
+      } else if (num % 3 == 0) {
+        results.innerHTML += 
+        `
+          <div class="fizz-buzz-item fizz">
+          fizz
+          </div>
+        `;
+      } else {
+        results.innerHTML += 
+        `
+          <div class="fizz-buzz-item">
+        ` + num + 
+        `
+          </div>
+        `;
+      }
+    }
+  })
+
+}
+
+function init() {
+  getFizz();
+  deleteFizz();
+}
+
+init()
